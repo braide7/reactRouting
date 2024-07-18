890b9b8264f457ab1a7f677e64b64237a4be565e
Braidenp <braidenp17@gmail.com>
2024-07-18 17:07:40 -0600

    added react-router

---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+
+import MainNavigation from "../components/MainNavigation"
+
+export default function ErrorPage(){
+
+    return(
+        <>  
+        <MainNavigation/>
+        <main>
+            <h1>An error Occurred!</h1>
+            <p>Could not findt his page!</p>
+        </main>
+        </>
+    )
+}
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom"
+
+export default function ProductDetailPage() {
+    const params = useParams();
+
+ 
+    return (
+        <>
+            <h1>Product Details</h1>
+            <p>{params.productId}</p>
+        </>
+    )
+}
